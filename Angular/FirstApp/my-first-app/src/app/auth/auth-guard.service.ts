1ff06f9db3937d22610de7a86ec8c9b5609dd72d
Oleg Galtsev <ogaltsev@gmail.com>
2019-02-10 14:13:09 +0300

    load and available only if signin

--- Angular/FirstApp/my-first-app/src/app/auth/auth-guard.service.ts
+++ Angular/FirstApp/my-first-app/src/app/auth/auth-guard.service.ts
@@ -1,11 +1,15 @@
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment} from '@angular/router';
 import {AuthService} from './auth.service';
 import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
    constructor(private authService: AuthService) {}
    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.authService.isAuthenticated();
+  }
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.isAuthenticated();
   }
 }
